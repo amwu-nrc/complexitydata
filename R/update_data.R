2d27a8536794120a355b7056cae6c544e6795d75
Hamish Gamble <hamish.gamble@flinders.edu.au>
2024-04-16 14:03:14 +0930

    Add a function to update state economic complexity for newly released data

---
+++ R/update_data.R
@@ -0,0 +1,11 @@
+#' Update Economic Complexity Data
+#'
+#' @return NULL
+#' @export
+#'
+update_data <- function() {
+  source("data-raw/services_data.R")
+  source("data-raw/state_global_export_data.R")
+  source("data-raw/state_economic_complexity.R")
+  devtools::document()
+}
