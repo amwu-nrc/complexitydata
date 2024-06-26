d3c931fc7aa56dd0c2ec1b75a47166f52f5a09ca
Hamish Gamble <hamish.gamble@flinders.edu.au>
2024-06-26 15:42:19 +0930

    Add atlas eci data

--- data-raw/atlas_data.R
+++ data-raw/atlas_data.R
@@ -3,7 +3,15 @@ library(haven)
 library(dplyr)
 
 country_hsproduct4digit_year <- read_dta("S:/CBGL-AITI/~General/~Data/Economic complexity/Atlas Data (2021)/country_hsproduct4digit_year.dta")
+
 atlas_pci <- country_hsproduct4digit_year |>
   distinct(year, hs_product_code, pci)
 
-usethis::use_data(atlas_pci, overwrite = TRUE)
+usethis::use_data(atlas_pci, overwrite = TRUE, compress = "xz")
+
+
+atlas_eci <- country_hsproduct4digit_year |>
+  distinct(year, loction_code, eci)
+
+usethis::use_data(atlas_eci, overwrite = TRUE, compress = "xz")
+
