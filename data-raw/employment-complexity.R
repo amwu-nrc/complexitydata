e1f01af3a7250fa6fde1c8595d74201432b65600
Hamish Gamble <hamish.gamble@flinders.edu.au>
2024-07-24 13:44:59 +0930

    Add sa3 industry-occupation employment

--- data-raw/employment-complexity.R
+++ data-raw/employment-complexity.R
@@ -1,5 +1,6 @@
 ## code to prepare `employment-complexity` dataset goes here
 library(rlang)
+library(tidyverse)
 
 
 
@@ -9,8 +10,8 @@ employment_complexity <- function(data, region, activity, digits, remove_totals 
   region_name <- paste({{region}}, "(POW)")
 
   activity_name <- switch(activity,
-                     "indp" = "INDP Industry of Employment",
-                     "occp" = "OCCP Occupation")
+                          "indp" = "INDP Industry of Employment",
+                          "occp" = "OCCP Occupation")
   activity_name <- paste0(digits, "-digit level ", activity_name)
 
   nmax <- suppressWarnings(min(which(is.na(readr::read_csv(name_repair = 'unique_quiet',data, show_col_types = F,skip = 9)$Count)))-1)
@@ -35,9 +36,21 @@ employment_complexity <- function(data, region, activity, digits, remove_totals 
 
 
 # sa3 ---------------------------------------------------------------------
+sa3_indp1_occp1 <- read_csv("data-raw/abs/sa3-pow-occp-indp-1-digit.csv",
+                            skip = 9,
+                            n_max = 99360,
+                            col_select = c(sa3 = "SA3 (POW)",
+                                           anzsic_division = "1-digit level INDP Industry of Employment",
+                                           anzsco_major = "1-digit level OCCP Occupation",
+                                           count = Count)) |>
+  filter(!anzsic_division %in% c("Inadequately described", "Not stated",
+                                 "Not applicable", "Total"),
+         !anzsco_major %in% c("Inadequately described", "Not stated",
+                              "Not applicable", "Total"),
+         sa3 != "Total")
 
 sa3_occp4 <- employment_complexity("data-raw/abs/sa3-pow-occp-4-digit.csv", region = "SA3", activity = "occp", digits = 4)
 sa3_indp4 <- employment_complexity("data-raw/abs/sa3-pow-indp-4-digit.csv", region = "SA3", activity = "indp", digits = 4)
 
 
-usethis::use_data(sa3_indp4, sa3_occp4, overwrite = TRUE, compress = "xz")
+usethis::use_data(sa3_indp1_occp1, sa3_indp4, sa3_occp4, overwrite = TRUE, compress = "xz")
