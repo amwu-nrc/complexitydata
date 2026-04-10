5c070654f369486de2b65a6ec928c575611b63b3
Hamish Gamble <hamish.gamble@gmail.com>
2026-04-10 16:31:00 +0930

    Calculate 6 digit data for 2024 using HS 12

--- data-raw/atlas_data6.R
+++ data-raw/atlas_data6.R
@@ -10,17 +10,19 @@ rankings <- read_csv(
   filter(year == 2024)
 
 atlas_raw_6 <- read_csv(
-  "data-raw/atlas_export_data/hs92_country_product_year_6.csv"
+  "data-raw/atlas_export_data/hs12_country_product_year_6.csv"
 ) |>
   filter(country_id %in% rankings$country_id, year == 2024)
 
 
-atlas_economic_complexity92_6 <- calculate_complexity(
+atlas_economic_complexity12_6 <- calculate_complexity(
   atlas_raw_6,
   region = "country_iso3_code",
-  product = "product_hs92_code",
+  product = "product_hs12_code",
   value = "export_value",
   year = 2024
 )
 
-usethis::use_data(atlas_economic_complexity92_6, overwrite = T, compress = "xz")
+atlas_economic_complexity12_6 <- atlas_economic_complexity12_6 |>
+  rename(location_code = country_iso3_code)
+usethis::use_data(atlas_economic_complexity12_6, overwrite = T, compress = "xz")
