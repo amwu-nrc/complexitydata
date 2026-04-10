6647d87c6c07bb047236b32aae5ab0b9c838feab
Hamish Gamble <hamish.gamble@gmail.com>
2026-04-10 13:25:11 +0930

    Calculate 6-digit complexity data for 2024 using discrete mcp

--- data-raw/atlas_data6.R
+++ data-raw/atlas_data6.R
@@ -4,13 +4,23 @@ library(dplyr)
 library(readr)
 devtools::load_all()
 
-rankings <- read_csv("data-raw/atlas_export_data/growth_proj_eci_rankings.csv")
+rankings <- read_csv(
+  "data-raw/atlas_export_data/growth_proj_eci_rankings.csv"
+) |>
+  filter(year == 2024)
 
-atlas_raw_6 <- read_csv("data-raw/atlas_export_data/hs92_country_product_year_6.csv") |>
-  filter(country_id %in% rankings$country_id)
+atlas_raw_6 <- read_csv(
+  "data-raw/atlas_export_data/hs92_country_product_year_6.csv"
+) |>
+  filter(country_id %in% rankings$country_id, year == 2024)
 
 
+atlas_economic_complexity92_6 <- calculate_complexity(
+  atlas_raw_6,
+  region = "country_iso3_code",
+  product = "product_hs92_code",
+  value = "export_value",
+  year = 2024
+)
 
-atlas_economic_complexity92_6 <- calculate_complexity_time_series(atlas_raw_6, years = unique(atlas_raw_6$year), region = "country_iso3_code", product = "product_hs92_code", value = "export_value")
-
-usethis::use_data(atlas_economic_complexity92_6, compress = "xz")
+usethis::use_data(atlas_economic_complexity92_6, overwrite = T, compress = "xz")
