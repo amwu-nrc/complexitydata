05a6aea76500222d18e43b6d460e345e5dd6932b
hamgamb <hamish.gamble@flinders.edu.au>
2026-04-16 11:14:30 +1000

    Create 4 and 6 digit parquet files

--- data-raw/atlas_data.R
+++ data-raw/atlas_data.R
@@ -4,27 +4,27 @@ library(dplyr)
 library(arrow)
 library(purrr)
 library(stringr)
-
+library(ecomplexity)
 atlas_trade_data <- function(data, classification) {
 
   classification <- str_split_1(classification, "_")
   trade_codec <- classification[1]
   digits <- classification[2]
 
-    data |>
-      select(contains("code"),
-             year,
-             export_value,
-             export_rca,
-             import_value,
-             global_market_share,
-             distance,
-             cog,
-             pci) |>
-      rename_with(\(x) str_remove(x, "hs[0-9]+_|sitc_"),
-                  .cols = starts_with("product")) |>
-      mutate(classification = trade_codec,
-             digits = digits)
+  data |>
+    select(contains("code"),
+           year,
+           export_value,
+           export_rca,
+           import_value,
+           global_market_share,
+           distance,
+           cog,
+           pci) |>
+    rename_with(\(x) str_remove(x, "hs[0-9]+_|sitc_"),
+                .cols = starts_with("product")) |>
+    mutate(classification = trade_codec,
+           digits = digits)
 
 
 }
@@ -51,7 +51,33 @@ country_product <- list(
 
 country_product_4 <- imap(.x = country_product,
                           .f = \(x, y) atlas_trade_data(x, classification = y))  |>
-  list_rbind() |>
-  group_by(classification) |>
-  write_dataset(path = "data", format = "parquet")
+  list_rbind()
+
+atlas_raw_6 <- read_csv("data-raw/atlas_export_data/hs22/hs22_country_product_year_6.csv")
+
+country_product_6 <- calculate_complexity_time_series(atlas_raw_6,
+                                                      years = unique(atlas_raw_6$year),
+                                                      region = "country_iso3_code",
+                                                      product = "product_hs22_code",
+                                                      value = "export_value") |>
+  mutate(classification = "hs22",
+         digits = "6",
+         distance = 1 - density) |>
+  select(country_iso3_code,
+         product_code = product_hs22_code,
+         year,
+         export_value,
+         export_rca = rca,
+         import_value,
+         global_market_share,
+         distance,
+         cog,
+         pci = product_complexity_index,
+         classification,
+         digits)
+
+bind_rows(country_product_4, country_product_6) |>
+  group_by(classification, digits) |>
+  write_dataset(path = "data",
+                format = "parquet")
 
