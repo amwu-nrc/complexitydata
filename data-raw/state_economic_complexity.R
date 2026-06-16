0829dcd149a87992e50702e8bb4c79027528fea6
Hamish Gamble <hamish.gamble@gmail.com>
2026-06-16 20:51:53 +1000

    Rename state columns to be consistent with national data

--- data-raw/state_economic_complexity.R
+++ data-raw/state_economic_complexity.R
@@ -13,19 +13,27 @@ data("combined_exports")
 state_data <- combined_exports |>
   filter(!location_code %in% c("ACT"))
 
-economic_complexity <- calculate_complexity_time_series(data = state_data,
-                                                              region = "location_code",
-                                                              product = "hs_product_code",
-                                                              value = "export_value",
-                                                              years = unique(state_data$year))
+economic_complexity <- calculate_complexity_time_series(
+  data = state_data,
+  region = "location_code",
+  product = "hs_product_code",
+  value = "export_value",
+  years = unique(state_data$year)
+)
 
 complexity_rank <- economic_complexity |>
   distinct(year, location_code, country_complexity_index) |>
-  mutate(eci_rank = order(order(country_complexity_index, decreasing = TRUE)), .by = year)
+  mutate(
+    eci_rank = order(order(country_complexity_index, decreasing = TRUE)),
+    .by = year
+  )
 
 state_economic_complexity <- economic_complexity |>
-  left_join(complexity_rank , by = join_by(year, location_code, country_complexity_index)) |>
-  filter(location_code %in% clean_state(1:8))
-
+  left_join(
+    complexity_rank,
+    by = join_by(year, location_code, country_complexity_index)
+  ) |>
+  filter(location_code %in% clean_state(1:8)) |>
+  rename(country_iso3_code = location_code)
 
 usethis::use_data(state_economic_complexity, compress = "xz", overwrite = TRUE)
